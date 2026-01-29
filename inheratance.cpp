fcbcc2c9ddaee687078b23c5cabd459be34abc72
Hemant Kumar Sah <sah20080116@gmail.com>
2026-01-29 11:17:43 +0530

    done

---
+++ inheratance.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// Base class
+class Animal {
+public:
+    void eat() {
+        cout<<"Animal is eating"<<endl;
+    }
+};
+
+class Dog: public Animal { // Derived class
+public:
+    void bark() {
+        cout<<"Dog is barking"<<endl;
+    }
+};
+
+int main(){
+    Dog myDog;
+    myDog.eat();  // Inherited from Animal class
+    myDog.bark(); // From Dog class
+
+    Animal myAnimal;
+    myAnimal.eat(); // From Animal class
+    return 0;
+}
